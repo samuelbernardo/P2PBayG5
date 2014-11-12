c6f60459b78472eefa0a222065e470fa3fd307f9
EdPassos <ed.passos@gmail.com>
2014-11-12 17:20:15 +0000

    p2pbay.sh obtem jar file and cfg file

--- scripts/p2pbay.sh
+++ scripts/p2pbay.sh
@@ -3,6 +3,6 @@ export MQ_NODES="$HOME/nodes.txt"
 export MQ_SLICE='istple_seprs5'
 export PATH="$HOME/codeploy:$PATH"
 
-multiquery 'wget -q http://web.ist.utl.pt/~ist165965/java.sh; chmod +x java.sh; ./java.sh'
-multiquery 'javac teste.java'
-multiquery 'java teste'
+multiquery 'wget -q http://web.ist.utl.pt/~ist165965/bootstrap.cfg'
+multiquery 'wget -q http://web.ist.utl.pt/~ist165965/P2PBay.jar'
+multiquery 'java -jar P2PBay.jar'
