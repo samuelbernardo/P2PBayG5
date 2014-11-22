de565fb7fcde3d051fab98d967c4ac1790318e13
EdPassos <ed.passos@gmail.com>
2014-11-22 00:16:30 +0000

    SignUp Strings

--- src/p2pbay/client/SysStrings.java
+++ src/p2pbay/client/SysStrings.java
@@ -1,9 +1,13 @@
 package p2pbay.client;
 
 public class SysStrings {
+
     public static final String
-            LOGIN = "Login",
-            LOGIN_SUCCESS = "Login com sucesso",
-            LOGIN_FAILED = "Login falhou",
-            VALUE = "Valor:";
+    LOGIN = "Login",
+    LOGIN_SUCCESS = "Login com sucesso",
+    LOGIN_FAILED = "Login falhou",
+    VALUE = "Valor:",
+    SIGNUP = "Sign up",
+    USER_REGISTERED = "User registado",
+    USER_NOT_REGISTERED = "User não registado";
 }
