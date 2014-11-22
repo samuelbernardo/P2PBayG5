dbe15d1384b186a32edb976a37f21575a4b09a87
EdPassos <ed.passos@gmail.com>
2014-11-22 00:20:17 +0000

    UserInteraction Strings

--- src/p2pbay/client/SysStrings.java
+++ src/p2pbay/client/SysStrings.java
@@ -3,11 +3,15 @@ package p2pbay.client;
 public class SysStrings {
 
     public static final String
-    LOGIN = "Login",
-    LOGIN_SUCCESS = "Login com sucesso",
-    LOGIN_FAILED = "Login falhou",
-    VALUE = "Valor:",
-    SIGNUP = "Sign up",
-    USER_REGISTERED = "User registado",
-    USER_NOT_REGISTERED = "User não registado";
+            LOGIN = "Login",
+            LOGIN_SUCCESS = "Login com sucesso",
+            LOGIN_FAILED = "Login falhou",
+            VALUE = "Valor:",
+            INVALID_VALUE = "O valor introduzido nao e valido.",
+            SIGNUP = "Sign up",
+            USER_REGISTERED = "User registado",
+            USER_NOT_REGISTERED = "User não registado",
+            INPUT_USERNAME = "Username:",
+            INPUT_PASSWORD = "Password:"
+    ;
 }
