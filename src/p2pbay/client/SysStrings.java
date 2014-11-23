255cd149d1af595f46eb9cf720d678da6256324a
AnaSLCaldeira <anaslcaldeira@gmail.com>
2014-11-23 15:31:01 +0000

    Implementação de pesquisas simples com NOT.

--- src/p2pbay/client/SysStrings.java
+++ src/p2pbay/client/SysStrings.java
@@ -7,7 +7,8 @@ public class SysStrings {
     LOGIN_FAILED = "Login falhou",
     VALUE = "Valor:",
     SEARCH = "Search:",
-    SEARCH_INVALIDOPERATOR = "ERROR! Invalid boolean operator: ",
+    SEARCH_INVALIDOPERATOR = "ERROR! Invalid operator: ",
+    SEARCH_IMPOSSIBLE = "ERROR! This type of search is impossible in a decentralized service",
     SEARCH_RESULTS = "Search results:",
     SEARCH_NORESULTS = "No results were found, please try different keywords",
     SEARCH_FAILED = "The search isn't well formed, please try again using prefix notation";
