7dd72dc5774ea511198cee5256bf3aebdf193d9f
AnaSLCaldeira <anaslcaldeira@gmail.com>
2014-11-23 13:16:34 +0000

    Pesquisa com expressões booleanas simples.

--- src/p2pbay/client/SysStrings.java
+++ src/p2pbay/client/SysStrings.java
@@ -2,8 +2,13 @@ package p2pbay.client;
 
 public class SysStrings {
     public static final String
-            LOGIN = "Login",
-            LOGIN_SUCCESS = "Login com sucesso",
-            LOGIN_FAILED = "Login falhou",
-            VALUE = "Valor:";
+    LOGIN = "Login",
+    LOGIN_SUCCESS = "Login com sucesso",
+    LOGIN_FAILED = "Login falhou",
+    VALUE = "Valor:",
+    SEARCH = "Search:",
+    SEARCH_INVALIDOPERATOR = "ERROR! Invalid boolean operator: ",
+    SEARCH_RESULTS = "Search results:",
+    SEARCH_NORESULTS = "No results were found, please try different keywords",
+    SEARCH_FAILED = "The search isn't well formed, please try again using prefix notation";
 }
